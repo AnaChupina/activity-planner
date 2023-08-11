63f846726e68b393540c7c4a2032cea37a8230f6
Anastasia_Chupina <anastasia_chupina@epam.com>
2023-08-12 01:36:49 +0400

    Initial commit

---
+++ src/test/java/com/nimbral/activity/ActivityPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nimbral.activity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivityPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
